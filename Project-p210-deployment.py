c6b57f2da0e499dc7c50ed871b372c5da896bea5
Sagar Srinivas <121102662+DS-SS-0410@users.noreply.github.com>
2023-05-11 13:59:38 +0530

    Update Project-p210-deployment.py

--- Project-p210-deployment.py
+++ Project-p210-deployment.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 import streamlit as st 
-pip install scikit-learn
+!pip install scikit-learn
 from sklearn.linear_model import LogisticRegression
 
 st.title('Fake Bills Classification: Logistic Regression')
